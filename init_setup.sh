873096b848aaf2ce6a7243ffcc1aa0e137394ea1
Nitin Kumar <nitin6201153107@gmail.com>
2022-04-16 13:10:07 +0530

    added

--- init_setup.sh
+++ init_setup.sh
@@ -1,19 +1,4 @@
-echo [$(date)]: "START"
-export _VERSION_=3.7
-echo [$(date)]: "creating environment with python ${_VERSION_}"
-conda create --prefix ./env python=${_VERSION_} -y
-echo [$(date)]: "activate environment"
+source create --prefix ./env python=3.7 -y
 source activate ./env
-echo [$(date)]: "install requirements"
-pip install -r requirements.txt
-echo [$(date)]: "git init"
-git init
-echo [$(date)]: "dvc init"
-dvc init
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
-echo [$(date)]: "END"
 
-# to remove everything -
-# rm -rf env/ .gitignore conda.yaml README.md .git/
+source env export > conda.yaml
