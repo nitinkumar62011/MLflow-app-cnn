058f078634fd36c7f7c7f668cbb71aa71f710c3b
Nitin Kumar <nitin6201153107@gmail.com>
2022-04-16 20:18:34 +0530

    added file

--- src/stage_00_template.py
+++ src/stage_00_template.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
 import random
+import requests as req
+import urllib.request as req
 
 
 STAGE = "STAGE_NAME" ## <<< change stage name 
@@ -17,23 +19,33 @@ logging.basicConfig(
     )
 
 
-def main(config_path, params_path):
+def main(config_path):
     ## read config files
     config = read_yaml(config_path)
-    params = read_yaml(params_path)
-    pass
+    URL = config["data"]["source_url"]
+    local_dir = config["data"]["local_dir"]
+    create_directories([local_dir])
 
+    data_file = config["data"]["data_file"]
+    data_file_path = os.path.join(local_dir, data_file)
+
+    
+    if not os.path.isfile(data_file_path):
+        logging.info("downloading started...")
+        filename, headers = req.urlretrieve(URL, data_file_path)
+        logging.info(f"filename:{filename} created with info \n{headers}")
+    else:
+        logging.info(f"filename:{data_file} already present")
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        main(config_path=parsed_args.config)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
