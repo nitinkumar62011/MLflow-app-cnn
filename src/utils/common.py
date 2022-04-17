d6cf607b3d1a411970593ce959e15842b916079c
Nitin Kumar <nitin6201153107@gmail.com>
2022-04-17 14:08:35 +0530

    added data managment file

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,18 +1,23 @@
 import os
+from zipfile import ZipFile
 import yaml
 import logging
 import time
 import pandas as pd
 import json
+from zipfile import ZipFile
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
+        print(content)
     logging.info(f"yaml file: {path_to_yaml} loaded successfully")
     return content
 
 def create_directories(path_to_directories: list) -> None:
+    print(f"creating directories: {path_to_directories}")
     for path in path_to_directories:
+        print(f"creating directory: {path}")
         os.makedirs(path, exist_ok=True)
         logging.info(f"created directory at: {path}")
 
@@ -22,3 +27,7 @@ def save_json(path: str, data: dict) -> None:
         json.dump(data, f, indent=4)
 
     logging.info(f"json file saved at: {path}")
+def unzip_file(source:str,dest:str) ->None:
+    with ZipFile(source, 'r') as zipObj:
+        zipObj.extractall(dest)
+    logging.info(f"unzipped file: {source} to: {dest}")
